4fc89e5b5f332a2498af3e184ee7f0f1567bf77e
Khushboo0511 <khushboochaudhary0511@gmail.com>
2024-03-20 22:59:06 +0530

    frontend

---
+++ defi-app-react/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import FeatureOne from './components/FeatureOne';
+function App() {
+  return (
+    <div className="home">
+      <Header />
+      <HeroSection />
+      <FeatureOne />
+    </div>
+  );
+}
+
+export default App;
